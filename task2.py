b4ba42db2fbe2097fbc3dbc9c88e315b7e15f50f
znxKream <133039965+znxKream@users.noreply.github.com>
2024-04-26 11:04:04 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,19 @@
+'''a = 1, 2, 3
+b = str(a)
+print(b)
+print(type(b))'''
+
+'''a = 1, 5.5,"hello"
+b = list(a)
+print(b)
+print(type(b))'''
+
+'''a = int(input('Введите целое число'))
+if a % 2:
+    print(f'Число {a} является нечетным')
+else:
+    print(f'Число {a} является четным')'''
+
+us = input('Введите строку:')
+num = len(us)
+print(f'Количество символов в строке: {num}')
